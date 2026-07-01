a35e71ceb3dc821f88339710b2209fd8b1e59e4d
diegoev309-lgtm <diego.ev.309gmail.com>
2026-07-01 08:06:41 -0500

    nueva actualizacion de templates productos y produccion

--- lzapp/home/views.py
+++ lzapp/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from producto.models import Producto
 
-def main(resquet):
-    return render(resquet, "masterpage.html")
+def main(request):
+    productos = Producto.objects.filter(disponibilidad=True)
+    return render(request, "masterpage.html", {"productos": productos})
 
 def user(request):
     return render(request, "users.html")
